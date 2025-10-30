90e740967c59d803922686f8c1ab34b286b6a1fd
Koushik25022005 <sripathikoushik244@gmail.com>
2025-10-30 18:01:48 +0400

    http basics

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,32 @@
-console.log('Express Tutorial')
+const http = require('http')
+const {readFileSync} = require('fs')
+
+//get all files
+const homePage = readFileSync('./navbar-app/index.html')
+
+const server = http.createServer((req, res)=>{
+    //console.log(req.method)
+    const url = req.url
+    console.log(url)
+    //home page
+    if (url == '/'){
+        res.writeHead(200, {'content-type':'text/html'})
+        res.write(homePage)
+        res.end()
+    }
+    //about page
+    else if (url == '/about'){
+        res.writeHead(200, {'content-type':'text/html'})
+        res.write('<h1> home page </h1>')
+        res.end()
+    }
+    //404
+    else {
+        res.writeHead(404, {'content-type':'text/html'})
+        res.write('<h1>Page Not Found</h1>')
+        res.end()
+    }
+    
+})
+
+server.listen(5000)
